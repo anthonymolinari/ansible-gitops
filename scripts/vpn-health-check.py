9062ffbf5f356595f652a29393bdbfa6865dae34
anthonymolinari <anthonymolinari106@gmail.com>
2024-12-09 08:01:23 -0800

    updates

--- scripts/vpn-health-check.py
+++ scripts/vpn-health-check.py
@@ -7,7 +7,8 @@ from docker.models.containers import Container
 import docker
 import json
 import requests as req
-import xml.etree.ElementTree as ET 
+import xml.etree.ElementTree as ET
+
 
 def parseConfig():
     config = None
@@ -15,6 +16,7 @@ def parseConfig():
         config_data = json.load(config_file)
         config = config_data
     return config
+
 
 def getKeyFromXML(path):
     tree = ET.parse(path)
@@ -24,19 +26,21 @@ def getKeyFromXML(path):
             return child.text
     return None
 
+
 def get_health(container: Container):
     api_client = APIClient()
     inspect_results = api_client.inspect_container(container.name)
     return inspect_results['State']['Health']['Status']
+
 
 def main():
 
     config = parseConfig()
 
     # get all api keys from service configs
-    prowlarr_api_key = getKeyFromXML(config['base_path'] + config['prowlarr']['config_path']) 
+    prowlarr_api_key = getKeyFromXML(config['base_path'] + config['prowlarr']['config_path'])
     sonarr_api_key = getKeyFromXML(config['base_path'] + config['sonarr']['config_path'])
-    radarr_api_key = getKeyFromXML(config['base_path'] + config['radarr']['config_path'])   
+    radarr_api_key = getKeyFromXML(config['base_path'] + config['radarr']['config_path'])
     # grab service urls from config
     prowlarr_url = config['prowlarr']['url']
     sonarr_url = config['sonarr']['url']
@@ -57,12 +61,12 @@ def main():
 
     vpn.restart()
 
-    while( get_health(vpn) != "healthy"): 
+    while get_health(vpn) != "healthy":
         sleep(1)
-    
+
     transmission.start()
     prowlarr.start()
-    sleep(60) # await vpn startup
+    sleep(60)  # await vpn startup
 
     # download client test connection
     req.post(f"{prowlarr_url}/api/v1/downloadclient/testall?apikey={prowlarr_api_key}")
@@ -73,6 +77,6 @@ def main():
     req.post(f"{sonarr_url}/api/v3/indexer/testall?apikey={sonarr_api_key}")
     req.post(f"{radarr_url}/api/v3/indexer/testall?apikey={radarr_api_key}")
 
+
 if __name__ == "__main__":
     main()
-
